c272b8c7052d771244a7739d65a52d3411c726a3
Soap <99285210+Soph055@users.noreply.github.com>
2022-06-09 14:39:43 -0400

    microbit code
    added mini 8 ball game

--- main.py
+++ main.py
@@ -2,17 +2,31 @@ import music
 import speech
 from microbit import sleep
 from microbit import *
+import random
+
 
 Song = "In The Hall Of The Mountain King"
 
 while True:
+    
+    ballAnswers = [ "no way",
+                       "yup",
+                       "nah",
+                       "never",
+                       "ye",
+                       "YES",
+                       "prob not",
+                       "maybe",
+                       "no",]
+     
+    display.show("8")
+    if accelerometer.was_gesture("shake"):
+         display.clear()
+         sleep(500)
+         display.scroll(random.choice(ballAnswers))
+         
      
     if button_a.was_pressed():
-        # The say method attempts to convert English into phonemes.
-        speech.say("Let me play you a song.")
-        sleep(500)
-        speech.say("Wish me luck!")
-        sleep(500)
         
         tune = ["A3:2", "B3:2", "C4:2", "D4:2", "E4:2", "C4:2", "E4:4", "D#4:2", "B3:2","D#4:4",
                 "D4:2", "B3:2","D4:4", "A3:2", "B3:2", "C4:2", "D4:2", "E4:2", "C4:2", "E4:2",
@@ -24,5 +38,9 @@ while True:
         display.scroll(str(Song), wait=False, loop=False)
         music.play(tune)
         
-    if button_a.was_pressed():
+    if button_a.was_pressed(): 
+       pass
+        
+        
+        
         
